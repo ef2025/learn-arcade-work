a3f4097a48ab515af799d54643633092cbf94030
ef2025 <ef2025@email.vccs.edu>
2023-01-19 12:20:43 -0500

    Updated Lab 1

--- Lab 01 - First Program/lab_01.py
+++ Lab 01 - First Program/lab_01.py
@@ -2,6 +2,5 @@ print("Hello from CSC221!")
 print("""This is a test
 using triple quotes for
 multiple lines.""")
-print("By using a slash " I can show a double quote.")"
 print("By using a slash: \" I can show a double quote!")
 print('This is the last quote.')
